e699e295aad795fd3184a43766b39fd0eee7cbf7
harryyking <arthurharry06@gmail.com>
2025-09-29 01:50:02 +0000

    ss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
-import {Geist, Geist_Mono} from 'next/font/google'
+import {Geist} from 'next/font/google'
 import { Suspense } from "react"
 import "./globals.css"
 
